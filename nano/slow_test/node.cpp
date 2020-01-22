4cbe1a54f1cb763d978eb168fc24df6ceb7196a9
wezrule <wezrule@hotmail.com>
2020-01-22 19:37:48 +0000

    Slow test again

--- nano/slow_test/node.cpp
+++ nano/slow_test/node.cpp
@@ -513,9 +513,17 @@ TEST (confirmation_height, many_accounts_single_confirmation)
 		nano::confirmation_height_info confirmation_height_info;
 		ASSERT_FALSE (node->store.confirmation_height_get (transaction, account, confirmation_height_info));
 		ASSERT_EQ (count, confirmation_height_info.height);
-		ASSERT_EQ (confirmation_height_info.height, node->ledger.cache.cemented_count);
 		ASSERT_EQ (count, account_info.block_count);
 	}
+
+	auto cemented_count = 0;
+	for (auto i (node->ledger.store.confirmation_height_begin (transaction)), n (node->ledger.store.confirmation_height_end ()); i != n; ++i)
+	{
+		cemented_count += i->second.height;
+	}
+
+	auto count = node->ledger.cache.cemented_count.load ();
+	ASSERT_EQ (cemented_count, count);
 
 	ASSERT_EQ (node->ledger.stats.count (nano::stat::type::confirmation_height, nano::stat::detail::blocks_confirmed, nano::stat::dir::in), num_accounts * 2 - 2);
 }
@@ -566,6 +574,16 @@ TEST (confirmation_height, many_accounts_many_confirmations)
 	{
 		ASSERT_NO_ERROR (system.poll ());
 	}
+
+	auto transaction = node->store.tx_begin_read ();
+	auto cemented_count = 0;
+	for (auto i (node->ledger.store.confirmation_height_begin (transaction)), n (node->ledger.store.confirmation_height_end ()); i != n; ++i)
+	{
+		cemented_count += i->second.height;
+	}
+
+	auto count = node->ledger.cache.cemented_count.load ();
+	ASSERT_EQ (cemented_count, count);
 }
 
 TEST (confirmation_height, long_chains)
@@ -650,14 +668,22 @@ TEST (confirmation_height, long_chains)
 	nano::confirmation_height_info confirmation_height_info;
 	ASSERT_FALSE (node->store.confirmation_height_get (transaction, nano::test_genesis_key.pub, confirmation_height_info));
 	ASSERT_EQ (num_blocks + 2, confirmation_height_info.height);
-	ASSERT_EQ (confirmation_height_info.height, node->ledger.cache.cemented_count);
 	ASSERT_EQ (num_blocks + 3, account_info.block_count); // Includes the unpocketed send
 
 	ASSERT_FALSE (node->store.account_get (transaction, key1.pub, account_info));
 	ASSERT_FALSE (node->store.confirmation_height_get (transaction, key1.pub, confirmation_height_info));
 	ASSERT_EQ (num_blocks + 1, confirmation_height_info.height);
-	ASSERT_EQ (confirmation_height_info.height, node->ledger.cache.cemented_count);
 	ASSERT_EQ (num_blocks + 1, account_info.block_count);
+
+	auto cemented_count = 0;
+	for (auto i (node->ledger.store.confirmation_height_begin (transaction)), n (node->ledger.store.confirmation_height_end ()); i != n; ++i)
+	{
+		cemented_count += i->second.height;
+	}
+
+	auto count = node->ledger.cache.cemented_count.load ();
+	ASSERT_EQ (cemented_count, count);
+
 
 	ASSERT_EQ (node->ledger.stats.count (nano::stat::type::confirmation_height, nano::stat::detail::blocks_confirmed, nano::stat::dir::in), num_blocks * 2 + 2);
 }
